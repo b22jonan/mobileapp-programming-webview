807eb0e8b1beb1ea9ff7c9a4d0f69ec6ece77db6
Jonathan Antfolk <61019188+walsand987@users.noreply.github.com>
2023-04-04 10:39:36 +0200

    enabled javascript

--- app/src/main/java/com/example/webviewapp/MainActivity.java
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -12,11 +12,11 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void showExternalWebPage(){
+    public void showExternalWebPage() {
         // TODO: Add your code for showing external web page here
     }
 
-    public void showInternalWebPage(){
+    public void showInternalWebPage() {
         // TODO: Add your code for showing internal web page here
     }
 
@@ -31,31 +31,32 @@ public class MainActivity extends AppCompatActivity {
         WebViewClient my_webview_client = new WebViewClient();
         my_webview.setWebViewClient(my_webview_client);
 
-        
+        my_webview.getSettings().setJavaScriptEnabled(true);
+
         /*
-        * Rename your App. Tip: Values->Strings
-        * Enable Internet access for your App. Tip: Manifest
-        * Create a WebView element in the layout file main_activity.xml
-        * Give the WebView element ID "my_webview"
-        -- Commit and push to your github fork
-        * Create a private member variable called "myWebView" of type WebView
-        * Locate the WebView element created in step 1 using the ID created in step 2
-        * Create a new WebViewClient to attach to our WebView. This allows us to
-          browse the web inside our app.
-        -- Commit and push to your github fork
-        * Enable Javascript execution in your WebViewClient
-        * Enter the url to load in our WebView
-        -- Commit and push to your github fork
-        * Move the code that loads a URL into your WebView into the two methods
-          "showExternalWebPage()" and "showInternalWebPage()".
-        * Call the "showExternalWebPage()" / "showInternalWebPage()" methods
-          when you select menu options "External Web Page" or "Internal Web Page"
-          respectively
-        -- Commit and push to your github fork
-        * Take two screenshots using the "Take a screenshot" tool in the AVD
-           showing your App. One (1) screenshot showing your internal web page and
-           one (1) screenshot showing your external web page.
-        */
+         * Rename your App. Tip: Values->Strings
+         * Enable Internet access for your App. Tip: Manifest
+         * Create a WebView element in the layout file main_activity.xml
+         * Give the WebView element ID "my_webview"
+         * -- Commit and push to your github fork
+         * Create a private member variable called "myWebView" of type WebView
+         * Locate the WebView element created in step 1 using the ID created in step 2
+         * Create a new WebViewClient to attach to our WebView. This allows us to
+         * browse the web inside our app.
+         * -- Commit and push to your github fork
+         * Enable Javascript execution in your WebViewClient
+         * Enter the url to load in our WebView
+         * -- Commit and push to your github fork
+         * Move the code that loads a URL into your WebView into the two methods
+         * "showExternalWebPage()" and "showInternalWebPage()".
+         * Call the "showExternalWebPage()" / "showInternalWebPage()" methods
+         * when you select menu options "External Web Page" or "Internal Web Page"
+         * respectively
+         * -- Commit and push to your github fork
+         * Take two screenshots using the "Take a screenshot" tool in the AVD
+         * showing your App. One (1) screenshot showing your internal web page and
+         * one (1) screenshot showing your external web page.
+         */
     }
 
     @Override
@@ -72,14 +73,14 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+        // noinspection SimplifiableIfStatement
         if (id == R.id.action_external_web) {
-            Log.d("==>","Will display external web page");
+            Log.d("==>", "Will display external web page");
             return true;
         }
 
         if (id == R.id.action_internal_web) {
-            Log.d("==>","Will display internal web page");
+            Log.d("==>", "Will display internal web page");
             return true;
         }
 
